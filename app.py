792bd300e37ed0632ce7f66823c4a4f6ce66ff5d
Berk Orbay <berkorbay@users.noreply.github.com>
2021-08-14 11:55:36 +0300

    added medium story link

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import requests
-import json
+    import json
 import os
 import pandas as pd
 from datetime import datetime, timedelta
@@ -13,3 +13,12 @@ st.session_state['cur_date'] = st.date_input("Choose Date",value=datetime.today(
 price_df = get_prices(str(st.session_state['cur_date']))
 
 st.table(price_df)
+
+st.header("Notes & References")
+
+st.markdown("""
+- Read the story [here](https://medium.com/berk-orbay/streamlit-a-new-way-to-prototype-interactive-ui-in-python-92815dfd437b).
+- This is a simple demo to use [Streamlit Share](https://streamlit.io/sharing)
+- Streamlit Share basically takes the app code from a public Github repo ([Click to see source repo](https://github.com/berkorbay/st-izmir-open-data)). You can easily fork it and work on your own project.
+- Data is taken with WEB API from [Izmir Municipality Open Data Portal](https://acikveri.bizizmir.com)
+""")
