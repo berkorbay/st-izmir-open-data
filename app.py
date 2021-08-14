fbbbbfd1732c11513e89ceb12ce680a69a7588db
Berk Orbay <berkorbay@users.noreply.github.com>
2021-08-14 12:07:43 +0300

    layout changes and explanations

--- app.py
+++ app.py
@@ -12,6 +12,17 @@ st.set_page_config(
 )
 
 st.title("Fruit & Vegetable Prices at Izmir Wholesale Market")
+st.header("Notes & References")
+
+st.markdown("""
+- Read the story [here](https://medium.com/berk-orbay/streamlit-a-new-way-to-prototype-interactive-ui-in-python-92815dfd437b).
+- This is a simple demo to use [Streamlit Share](https://streamlit.io/sharing)
+- Streamlit Share basically takes the app code from a public Github repo ([Click to see source repo](https://github.com/berkorbay/st-izmir-open-data)). You can easily fork it and work on your own project.
+- Data is taken with WEB API from [Izmir Municipality Open Data Portal](https://acikveri.bizizmir.com)
+""")
+
+st.header("App")
+st.markdown("Usage: Choose a date and the app will bring a table of fruit and vegetable prices from Izmir Municipality Open Data Portal.")
 
 col1, col2 = st.beta_columns([2,11])
 
@@ -21,11 +32,4 @@ price_df = get_prices(str(st.session_state['cur_date']))
 
 st.table(price_df)
 
-st.header("Notes & References")
 
-st.markdown("""
-- Read the story [here](https://medium.com/berk-orbay/streamlit-a-new-way-to-prototype-interactive-ui-in-python-92815dfd437b).
-- This is a simple demo to use [Streamlit Share](https://streamlit.io/sharing)
-- Streamlit Share basically takes the app code from a public Github repo ([Click to see source repo](https://github.com/berkorbay/st-izmir-open-data)). You can easily fork it and work on your own project.
-- Data is taken with WEB API from [Izmir Municipality Open Data Portal](https://acikveri.bizizmir.com)
-""")
